282e771b78d085d811b6075f494cc321b6067c52
Hp <aasd>
2019-08-25 15:19:48 +0200

    1st commit

---
+++ src/main/java/CucStep.java
@@ -0,0 +1,36 @@
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CucStep {
+    @Given("^user, not loged in, present on the website$")
+    public void userNotLogedInPresentOnTheWebsite() {
+    }
+
+    @When("^user clicks on MATERIALS$")
+    public void userClicksOnMATERIALS() {
+    }
+
+    @And("^clicks on Download files$")
+    public void clicksOnDownloadFiles() {
+    }
+
+    @And("^selects given file$")
+    public void selectsGivenFile() {
+        
+    }
+
+    @And("^clicks on download icon$")
+    public void clicksOnDownloadIcon() {
+        
+    }
+
+    @Then("^the website will open download window$")
+    public void theWebsiteWillOpenDownloadWindow() {
+    }
+
+    @And("^the file will be downloaded$")
+    public void theFileWillBeDownloaded() {
+    }
+}
